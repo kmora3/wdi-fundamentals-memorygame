9a40eb0e892d1749073e1eb7790afb2c7e8b7ece
Kris Mora <krismora@KMora.local>
2016-11-11 01:36:19 -0800

    Chapters 9,10,11

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,24 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
+var cards = ['queen', 'queen', 'king', 'king'];
 
-if (cardOne === cardThree) {
-	alert("You found a match!");
+var cardsInPlay=[]
+
+// if (cardOne === cardThree) {
+// 	alert("You found a match!");
 } 
-else{
-alert ("Sorry, try again.");
-}
+// else{
+// alert ("Sorry, try again.");
+// }
 
+var board = document.getElementById('game-board');
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+
+    var cardElement = document.createElement('div');
+
+    cardElement.className = 'card';
+
+
+    board.appendChild(cardElement);
+  }
